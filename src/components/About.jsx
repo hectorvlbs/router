6829a709f011ca74fd4053e6fcbdfc911fd21ac1
hectorvlbs <44859441+hectorvlbs@users.noreply.github.com>
2021-03-13 18:32:41 -0700

    Hook useEffect

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 
 const About = () => {
+
+    const [team, setTeam] = React.useState([])
+
+
+    React.useEffect(() => {
+        console.log('useEffect')
+        getAPI()
+    }, [])
+
+    const getAPI = async () => {    
+        const Data = await fetch('https://jsonplaceholder.typicode.com/users')
+        const Users = await Data.json()
+        setTeam(Users)
+    }
+
     return (
         <div>
             <h1>Nosotros</h1>
+            <ul>
+                {
+                    team.map(item => (
+                        <li key={item.id}>{item.name} - {item.email}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
